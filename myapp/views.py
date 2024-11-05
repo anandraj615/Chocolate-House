222fb5455ef711f36cfe23eaa0f061999edc0222
anandraj615 <98384574+anandraj615@users.noreply.github.com>
2024-11-05 23:21:45 +0530

    second commit

--- myapp/views.py
+++ myapp/views.py
@@ -3,15 +3,22 @@ from django.views.generic import ListView,DetailView,CreateView
 from .models import Flavour,Ingredient,CustomerSuggestion
 from django.contrib import messages
 
-# Create your views here.
+from django.views.generic import ListView
+from .models import Flavour  # Replace with your actual model name
+
 class FlavorListView(ListView):
     model = Flavour
-    temp_name='myapp/flavour_list.html'
+    template_name = 'myapp/flavour_list.html'  
     context_object_name = 'flavors'
     
     def get_queryset(self):
-        return Flavour.objects.filter(is_active=True)
-    
+        queryset = Flavour.objects.filter(is_active=True)
+        season_name = self.request.GET.get('season')
+        
+        if season_name:
+            queryset = queryset.filter(season__name__iexact=season_name)
+        
+        return queryset
 class IngredientListView(ListView):
     model = Ingredient
     temp_name = 'myapp/ingredient_list.html'
@@ -23,10 +30,3 @@ class CustomerSuggestionCreateView(CreateView):
     fields = ['customer_name','flavour_name', 'description', 'allergies']
     success_url = '/'
 
-# class SeasonalFlavorsView(ListView):
-#     model = Flavour
-#     template_name = 'myapp/seasonal_flavors.html'
-#     context_object_name = 'flavors'  
-#     def get_queryset(self):
-#         selected_season = self.request.GET.get('season', 'spring')  
-#         return Flavour.objects.filter(season__name=selected_season)  
